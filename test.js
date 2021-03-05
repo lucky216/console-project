eb69f1149b60ccd4a34242fde3ee1b3e060b3dba
lucky216 <80086107+lucky216@users.noreply.github.com>
2021-03-04 23:54:44 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log("My first console project");
+var firstname = "lakshmi";
+var lastname = "samineni";
+console.log(firstname.concat(lastname));
+var num1 = 2;
+var num2 = 4;
+console.log(num1+num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1-num2);
+
+
+
+
+    
+
+    
